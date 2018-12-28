b3affcde1b6fe84438202e6e368ed255f3eb803a
LemonTea387 <JunJieLow08@gmail.com>
2018-12-28 16:19:54 +0800

    3rd Commit

--- core/src/com/nugget/flappywings/States/MenuState.java
+++ core/src/com/nugget/flappywings/States/MenuState.java
@@ -10,6 +10,7 @@ public class MenuState extends State {
     private Texture playButton;
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        cam.setToOrtho(false, (FlappyBird.WIDTH/2),(FlappyBird.HEIGHT/2));
         background = new Texture("bg.png");
         playButton = new Texture("playButton.png");
     }
@@ -28,9 +29,10 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(background,0,0,FlappyBird.WIDTH,FlappyBird.HEIGHT);
-        sb.draw(playButton,(FlappyBird.WIDTH/2) - (playButton.getWidth()/2),(FlappyBird.HEIGHT / 2));
+        sb.draw(background,0,0);
+        sb.draw(playButton,cam.position.x - playButton.getWidth()/2,cam.position.y );
         sb.end();
     }
 
